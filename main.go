f4e66436635fb65daa2cfb7d317ca66fb6074e58
Pasi Ollikainen <pasi.ollikainen@pm.me>
2021-11-20 21:02:36 +0200

    2.09

--- main.go
+++ main.go
@@ -18,12 +18,15 @@ func main() {
 	if err != nil {
 		log.Fatalf("getting wiki url failed: %s", err)
 	}
-	dailyWiki := Todo{task:	fmt.Sprintf("%s://%s%s", r.Request.URL.Scheme, r.Request.URL.Host, r.Request.URL.Path)}
+	dailyWiki := Todo{task: fmt.Sprintf("%s://%s%s", r.Request.URL.Scheme, r.Request.URL.Host, r.Request.URL.Path)}
 
 	body, err := json.Marshal(dailyWiki)
 	if err != nil {
 		log.Fatalf("marshalling dailyWiki failed: %s", err)
 	}
-	http.Post(fmt.Sprintf("%s/todo",os.Getenv("TODO_HOST")),"application/json", bytes.NewBuffer(body))
+	r, err = http.Post(fmt.Sprintf("%s/todo", os.Getenv("TODO_HOST")), "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		log.Fatalf("posting dailyWiki failed: %s", err)
+	}
 	log.Printf("created task for wikiarticle: %s", dailyWiki)
 }
