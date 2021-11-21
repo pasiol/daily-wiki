c5de61c00ba6594b3fe9c0c49e915f89834bb064
Pasi Ollikainen <pasi.ollikainen@pm.me>
2021-11-21 07:28:53 +0200

    2.09

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module daily-wiki
 
 go 1.16
+
+require github.com/joho/godotenv v1.4.0
--- main.go
+++ main.go
@@ -4,29 +4,54 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/joho/godotenv"
+	"io"
 	"log"
 	"net/http"
 	"os"
 )
 
-type Todo struct {
-	task string
-}
-
 func main() {
+	type payload struct {
+		Task string `json:"task"`
+	}
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Print("Reading environment failed.")
+	}
 	r, err := http.Get("https://en.wikipedia.org/wiki/Special:Random")
 	if err != nil {
 		log.Fatalf("getting wiki url failed: %s", err)
 	}
-	dailyWiki := Todo{task: fmt.Sprintf("%s://%s%s", r.Request.URL.Scheme, r.Request.URL.Host, r.Request.URL.Path)}
 
-	body, err := json.Marshal(dailyWiki)
-	if err != nil {
-		log.Fatalf("marshalling dailyWiki failed: %s", err)
+	task := fmt.Sprintf("%s://%s%s", r.Request.URL.Scheme, r.Request.URL.Host, r.Request.URL.Path)
+
+	data := payload{
+		Task: task,
 	}
-	r, err = http.Post(fmt.Sprintf("%s/todo", os.Getenv("TODO_HOST")), "application/json", bytes.NewBuffer(body))
+	payloadBytes, _ := json.Marshal(data)
+
+	body := bytes.NewReader(payloadBytes)
+
+	req, err := http.NewRequest("POST", os.Getenv("TODO_HOST"), body)
 	if err != nil {
-		log.Fatalf("posting dailyWiki failed: %s", err)
+		// handle err
 	}
-	log.Printf("created task for wikiarticle: %s", dailyWiki)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("request failed: %s", err)
+	}
+	if resp != nil {
+		log.Printf("request statuscode %d %s", resp.StatusCode, resp.Status)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatalf("closing request failed: %s", err)
+		}
+	}(resp.Body)
+
 }
